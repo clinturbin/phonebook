37d09c52b64efadf312eca547990e8b2fc30d01b
clinturbin <clinturbin@gmail.com>
2018-08-23 12:04:05 -0400

    Start of PosetgreSQL version includes schema and seeds sql file to create beginner data

---
+++ backend-phone-book/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE contacts
+(  
+    id        character varying(255) NOT NULL,
+    name      character varying(255) NOT NULL,
+    phone     character varying(255) NOT NULL,
+    address   text,
+    email     character varying(255)
+);
---
+++ backend-phone-book/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO contacts (id, name, phone, address, email) VALUES
+('30022', 'Test1', '111-111-1111', '111 Main St', '1@test.com'),
+('80927', 'Test2', '222-222-2222', '222 Main St', '2@test.com');
